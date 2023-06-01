be1b695fc730ef98e2bf70b0703b9c37be9e476e
Andres Briñez <brinezlopez08@gmail.com>
2023-06-01 13:35:38 -0500

    Crear clase jugador

---
+++ Poo/src/Taller1/Clases/Jugador.java
@@ -0,0 +1,24 @@
+package Taller1.Clases;
+
+public class Jugador {
+
+    // Atributos
+    private String nombre;
+
+    // Constructor de la clase
+    public Jugador(String nombre) {
+        this.nombre = nombre;
+
+    }
+
+    // Getters y setters para los atributos de la clase
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+
+}
