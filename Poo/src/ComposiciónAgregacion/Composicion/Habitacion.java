a67e679bc73cc65d41a6a9a2fa2daf0c33471e7c
andres briñez <brinezlopez08@gmail.com>
2023-06-08 17:37:35 -0500

    Añadir el código de la clase
    Signed-off-by: Andres Briñez <brinezlopez08@gmail.com>
    Signed-off-by: andres briñez <brinezlopez08@gmail.com>

--- Poo/src/ComposiciónAgregacion/Composicion/Habitacion.java
+++ Poo/src/ComposiciónAgregacion/Composicion/Habitacion.java
@@ -1,4 +1,21 @@
 package ComposiciónAgregacion.Composicion;
 
 public class Habitacion {
+
+    private int numero;
+    private double tamano;
+
+    public Habitacion(int numero, double tamano) {
+        this.numero = numero;
+        this.tamano = tamano;
+    }
+
+    public int getNumero() {
+        return numero;
+    }
+
+    public double getTamano() {
+        return tamano;
+    }
+
 }
