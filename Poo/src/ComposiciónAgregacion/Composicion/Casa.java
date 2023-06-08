e3a81d088084c5cbe8290ed34b0f86bde21cddc3
andres briñez <brinezlopez08@gmail.com>
2023-06-08 18:10:58 -0500

    Añadir codigo de la clase
    Signed-off-by: Andres Briñez <brinezlopez08@gmail.com>
    Signed-off-by: andres briñez <brinezlopez08@gmail.com>

--- Poo/src/ComposiciónAgregacion/Composicion/Casa.java
+++ Poo/src/ComposiciónAgregacion/Composicion/Casa.java
@@ -1,4 +1,60 @@
 package ComposiciónAgregacion.Composicion;
 
 public class Casa {
+
+    // Atributos
+    private Habitacion[] habitaciones;
+    private int contador;
+    private final int CAPACIDAD_MAXIMA = 100; // Constante por lo que se usa final para declararla
+
+
+    public Casa() {
+
+        //Inicializa los valores
+        habitaciones = new Habitacion[CAPACIDAD_MAXIMA]; // Arreglo de habitaciones con la capacidad máxima
+        contador = 0;
+    }
+
+
+    public void agregarHabitacion(Habitacion habitacion) {
+
+        // Si hay capacidad disponible para agregar una habitación a la casa
+
+        if (contador < CAPACIDAD_MAXIMA) {
+
+            // Agrega la habitación al arreglo de habitaciones
+            habitaciones[contador] = habitacion;
+            contador++;
+
+        } else
+
+            // Si no hay capacidad disponible, muestra un mensaje de error
+            System.out.println("No se  puede agregar mas  habitaciones  porque se alcanzó la capacidad disponible.");
+
+    }
+
+
+    public void destruirCasa() {
+
+        habitaciones = new Habitacion[CAPACIDAD_MAXIMA];  // Crea un nuevo arreglo de habitaciones que elimina todas las habitaciones de la casa  porque se inicializa de  nuevo el arreglo
+        contador = 0;    // Reinicia el contador
+
+    }
+
+
+    public void mostrarInformacionCasa() {
+
+        if (contador == 0) {
+            System.out.println("No hay habitaciones creadas");
+        } else {
+            // Recorre todas las habitaciones en el arreglo de habitaciones para mostrar su informacion
+
+            for (int i = 0; i < contador; i++) {
+
+                System.out.println("Habitación  #" + habitaciones[i].getNumero() + ": " + habitaciones[i].getTamano() + " metros cuadrados.");
+
+            }
+        }
+    }
+
 }
--- Poo/src/ComposiciónAgregacion/Composicion/Main.java
+++ Poo/src/ComposiciónAgregacion/Composicion/Main.java
@@ -4,5 +4,31 @@ public class Main {
 
     public static void main(String[] args) {
 
+        Casa casa = new Casa();
+
+        //Crear instancia
+        Habitacion habitacion1 = new Habitacion(1, 10.6);
+        Habitacion habitacion2 = new Habitacion(2, 20.1);
+        Habitacion habitacion3 = new Habitacion(3, 30.3);
+        Habitacion habitacion4 = new Habitacion(4, 40.2);
+
+        // Agregar las habitaciones creadas a la casa
+        casa.agregarHabitacion(habitacion1);
+        casa.agregarHabitacion(habitacion2);
+        casa.agregarHabitacion(habitacion3);
+        casa.agregarHabitacion(habitacion4);
+
+        // Mostrar la información de la casa
+        System.out.println("Información de la casa:");
+        casa.mostrarInformacionCasa();
+        System.out.println();
+
+        // Destruir la casa
+        casa.destruirCasa();
+
+        System.out.println("Información de la casa:");
+        casa.mostrarInformacionCasa();
+
+
     }
 }
