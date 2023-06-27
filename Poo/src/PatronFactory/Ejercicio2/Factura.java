e381d2bcff873621a24a5d06ca0289a1329bb6ed
andres briñez <brinezlopez08@gmail.com>
2023-06-26 21:35:59 -0500

    Create Factura.java
    Clase padre

---
+++ Poo/src/PatronFactory/Ejercicio2/Factura.java
@@ -0,0 +1,12 @@
+package PatronFactory.Ejercicio2;
+
+// Define el comportamiento de  todas las facturas de la empresa
+public interface Factura {
+
+    // Metodo que calcula el total de la factura
+    void calcularValorFactura();
+
+    void mostrarFactura();
+
+
+}
